ea8367bca9d26a216e984f038842895159d2a4f0
Robert Perez <robertper280@gmail.com>
2023-09-10 19:09:10 -0700

    Player, Player Movement, Background

---
+++ Prototype2/Assets/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class MouseFollow : MonoBehaviour
+{
+    public float moveSpeed = 5.0f; // Adjust this value to control the movement speed.
+
+    private void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Get the mouse position in world coordinates.
+
+        // Make sure the object stays in the Z-plane and follows the mouse horizontally and vertically.
+        mousePosition.z = transform.position.z;
+
+        // Clamp the object's position to stay within the screen boundaries.
+        Vector3 clampedPosition = new Vector3(
+            Mathf.Clamp(mousePosition.x, ScreenUtils.ScreenMin.x, ScreenUtils.ScreenMax.x),
+            Mathf.Clamp(mousePosition.y, ScreenUtils.ScreenMin.y, ScreenUtils.ScreenMax.y),
+            transform.position.z
+        );
+
+        // Move the object towards the clamped mouse position.
+        transform.position = Vector3.MoveTowards(transform.position, clampedPosition, moveSpeed * Time.deltaTime);
+    }
+}
---
+++ Prototype2/Assets/ScreenUtils.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public static class ScreenUtils
+{
+    public static Vector2 ScreenMin
+    {
+        get
+        {
+            Vector2 min = Camera.main.ScreenToWorldPoint(Vector3.zero);
+            min.x += 0.5f; // Adjust this value to fit your object's size
+            min.y += 0.5f; // Adjust this value to fit your object's size
+            return min;
+        }
+    }
+
+    public static Vector2 ScreenMax
+    {
+        get
+        {
+            Vector2 max = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+            max.x -= 0.5f; // Adjust this value to fit your object's size
+            max.y -= 0.5f; // Adjust this value to fit your object's size
+            return max;
+        }
+    }
+}
